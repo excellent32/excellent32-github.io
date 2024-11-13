9d45523b0818f2eabe9e5058a7ba43ac3051f3dc
pwl <weili.pei@cmexpro.com>
2024-11-13 15:58:49 +0800

    code clean

--- src/app/(thrid-party)/apple/page.tsx
+++ src/app/(thrid-party)/apple/page.tsx
@@ -31,10 +31,10 @@ export default function Page() {
     await loadAppleAuthScript();
 
     AppleID.auth.init({
-      // response_type: 'code',
+      // response_type: 'id_token',
       clientId: 'service.pulse.social',
       scope: 'openid',
-      redirectURI: 'https://dev12.pulse.social:8080/callback',
+      redirectURI: `${location.origin}/callback`,
       // state: 'initial',
       nonce: '21208298071267998750675144691611999301006655472014769051443385150345422400305',
       usePopup : true
